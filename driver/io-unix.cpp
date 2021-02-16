22221630c97d936c6fcf2a217c8a6a4b3df31fd7
keldu.magnus <git@keldu.de>
2021-02-16 18:31:17 +0100

    move problems fixed

--- driver/io-unix.cpp
+++ driver/io-unix.cpp
@@ -307,9 +307,11 @@ AsyncIoContext setupAsyncIo() {
 	Own<UnixEventPort> prt = heap<UnixEventPort>();
 	UnixEventPort &prt_ref = *prt;
 	EventLoop event_loop{std::move(prt)};
-	EventLoop &loop_ref = event_loop;
+
 	Own<UnixAsyncIoProvider> io_provider =
 		heap<UnixAsyncIoProvider>(prt_ref, std::move(event_loop));
+
+	EventLoop &loop_ref = io_provider->eventLoop();
 
 	return {std::move(io_provider), loop_ref, prt_ref};
 }
