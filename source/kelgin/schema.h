cb88ff9434d909545288c5e6a836eb7d595581d1
keldu.magnus <git@keldu.de>
2021-11-11 00:20:21 +0100

    schema file

---
+++ source/kelgin/schema.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace gin {
+namespace schema {
+template<typename... T>
+struct Struct {
+};
+
+template<typename... T>
+struct Union {
+};
+
+template<typename... T>
+struct Array {
+};
+
+struct String {};
+}
+}
