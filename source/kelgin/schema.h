a2f73db9d9d9ce46a806c26d31aa80d147e774c2
keldu <mail@keldu.de>
2021-11-12 18:31:18 +0100

    schema language

--- source/kelgin/schema.h
+++ source/kelgin/schema.h
@@ -1,19 +1,25 @@
 #pragma once
 
+#include <kelgin/string_literal.h>
+
 namespace gin {
 namespace schema {
-template<typename... T>
-struct Struct {
-};
 
-template<typename... T>
-struct Union {
-};
+template <typename T, typename K> struct NamedMember;
 
-template<typename... T>
-struct Array {
-};
+template <typename T, typename C, C... Chars>
+struct NamedMember<T, StringLiteral<C, C...>> {};
+
+template <typename... T> struct Struct {};
+
+template <typename... V, typename... K> struct Struct<NamedMember<V, K>...> {};
+
+template <typename... T> struct Union {};
+
+template <typename... V, typename... K> struct Union<NamedMember<V, K>...> {};
+
+template <typename T> struct Array {};
 
 struct String {};
-}
-}
+} // namespace schema
+} // namespace gin
