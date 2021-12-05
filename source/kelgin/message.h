0df6555d08642e44827c1ac99ee46a3685b8e632
Claudius Holeksa <mail@keldu.de>
2021-12-05 18:21:50 +0100

    format and new Message types

--- source/kelgin/message.h
+++ source/kelgin/message.h
@@ -34,6 +34,10 @@ public:
  * singular types or has some form of encoding in a buffer present
  */
 
+
+/*
+ * Struct Message
+ */
 template <class... V, StringLiteral... Keys, class Container>
 class Message<schema::Struct<schema::NamedMember<V, Keys>...>, Container> final
 	: public MessageBase {
@@ -59,11 +63,18 @@ public:
 
 		Reader asReader() { return Reader{MessageType & message}; }
 
+		/*
+		 * Initialize a member by index
+		 */
 		template <size_t i> typename Container::Element<i>::Builder init() {
 			return typename Container::Element<i>::Builder{
 				message.container.get<i>()};
 		}
 
+		/*
+		 * Initialize a member by their name
+		 * This is the preferred method for schema::Struct messages
+		 */
 		template <StringLiteral Literal>
 		typename Container::Element<i>::Builder init() {
 			constexpr size_t i =
@@ -80,13 +91,20 @@ public:
 	public:
 		Reader(MessageType &msg) : message{msg} {}
 
-		Builder asBuilder() { return Builder{MessageType & message}; }
-
+		Builder asBuilder() { return Builder{message}; }
+		
+		/*
+		 * Get member by index
+		 */
 		template <size_t i> typename Container::Element<i>::Reader get() {
 			return typename Container::Element<i>::Reader{
 				message.container.get<i>()};
 		}
 
+		/*
+		 * Get member by name
+		 * This is the preferred method for schema::Struct messages
+		 */
 		template <StringLiteral Literal>
 		typename Container::Element<i>::Reader get() {
 			constexpr size_t i =
@@ -100,9 +118,79 @@ public:
 template <class T, size_t N, class Container>
 class Message<schema::Primitive<T, N>, Container> final : public MessageBase {
 private:
+	using SchemaType = schema::Primitive<T, N>;
+	using MessageType = Message<SchemaType, Container>;
+
 	Container container;
 
+	static_assert(std::is_same_v<SchemaType, typename Container::SchemaType>,
+
+	friend class Builder;
+	friend class Reader;
 public:
+	class Reader;
+	class Builder {
+	private:
+		MessageType &message;
+
+	public:
+		Builder(MessageType & msg) : message{msg} {}
+
+		Reader asReader() { return Reader{message}; }
+
+		void set(const T &value) { message.container.set(value); }
+	};
+
+	class Reader {
+	private:
+		MessageType &message;
+
+	public:
+		Reader(Message & msg) : message{msg} {}
+
+		Builder asBuilder() { return Builder{message}; }
+
+		const T &get() const { return message.container.get(); }
+	};
+};
+
+template <class Container>
+class Message<schema::String, Container> final : public MessageBase {
+private:
+	using SchemaType = schema::String;
+	using MessageType = Message<SchemaType, Container>;
+
+	Container container;
+
+	static_assert(std::is_same_v<SchemaType, typename Container::SchemaType>,
+
+	friend class Builder;
+	friend class Reader;
+public:
+	class Reader;
+	class Builder {
+	private:
+		MessageType &message;
+
+	public:
+		Builder(MessageType & msg) : message{msg} {}
+
+		Reader asReader() { return Reader{message}; }
+
+		void set(const std::string &str) { message.container.set(str); }
+	};
+
+	class Reader {
+	private:
+		MessageType &message;
+
+	public:
+		Reader(MessageType & msg) : message{msg} {}
+
+		Builder asBuilder() { return Builder{message}; }
+
+		std::string_view get() { return message.container.get(); }
+	}
 };
 
 class MessageReader {
