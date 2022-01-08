a8726327b05bc12653f2230c8ac6b8342b072951
keldu <mail@keldu.de>
2022-01-08 20:41:38 +0100

    added slightly longer buffer

--- test/io.cpp
+++ test/io.cpp
@@ -17,13 +17,17 @@ SAW_TEST("Io Socket Pair"){
 	
 	SocketPair& sp = err_or_sp.value();
 	
-	uint8_t buffer_out[3] = {1,2,3};
-	sp.stream[0]->write(buffer_out,3);
-	uint8_t buffer_in[3] = {0,0,0};
-	sp.stream[1]->read(buffer_in, 3);
+	uint8_t buffer_out[7] = {1,2,3,4,5,6,7};
+	sp.stream[0]->write(buffer_out,7);
+	uint8_t buffer_in[7] = {0,0,0,0,0,0,0};
+	sp.stream[1]->read(buffer_in, 7);
 
 	SAW_EXPECT(buffer_in[0] == 1, "Element 1 failed");
 	SAW_EXPECT(buffer_in[1] == 2, "Element 2 failed");
 	SAW_EXPECT(buffer_in[2] == 3, "Element 3 failed");
+	SAW_EXPECT(buffer_in[3] == 4, "Element 4 failed");
+	SAW_EXPECT(buffer_in[4] == 5, "Element 5 failed");
+	SAW_EXPECT(buffer_in[5] == 6, "Element 6 failed");
+	SAW_EXPECT(buffer_in[6] == 7, "Element 7 failed");
 }
 }
