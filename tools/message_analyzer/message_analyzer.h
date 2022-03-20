f44b6a1dc8875697b365331bb5c340b7932d770a
Claudius Holeksa <mail@keldu.de>
2022-03-20 22:25:17 +0100

    declaring a future template. it is not clean coding/vcs, but i am collecting features i want to freeze for later

---
+++ tools/message_analyzer/message_analyzer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <forstio/error.h>
+
+namespace saw {
+namespace tools {
+/// @todo implement from test code base
+template<typename Schema, typename Codec>
+Error cliMessageAnalyzer(Codec& codec, int argc, char** argv){
+	// Parse Args
+
+	// If read is required
+	// Read file into buffer
+	
+	// If read is required
+	// Decode file
+
+	// Execute commands
+
+	// If write is required
+	// Encode into buffer
+	// Write to file from buffer
+}
+}
+}
